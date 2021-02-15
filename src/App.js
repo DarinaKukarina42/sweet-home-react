7b1a827572a295deb90b06fd5410af0a939aa42f
Darina Kukarina <kukarina.darina42@gmail.com>
2021-02-14 19:58:14 -0500

    Add project files

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/"></Link>
+          <div id="logo-text">
+            <h3 id="adopt-me">SweetHome</h3>
+            <div id="line"></div>
+            <p id="shelter">Pet Shelter</p>
+          </div>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
